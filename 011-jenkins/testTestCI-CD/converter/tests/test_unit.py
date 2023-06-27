9fc8af9730615b7cb1042663f3bd89625a3149f0
Ubuntu <ubuntu@ip-172-31-23-248.eu-west-1.compute.internal>
2023-06-27 11:52:33 +0000

    aa

---
+++ 011-jenkins/testTestCI-CD/converter/tests/test_unit.py
@@ -0,0 +1,39 @@
+import unittest
+import requests
+import pytest
+import requests_mock
+
+from flask import url_for
+from flask_testing import TestCase
+
+from app import app
+
+from application.routes import birthDate 
+
+
+class TestInit(TestCase):
+    def create_app(self):
+        config_name = 'testing'
+        app.config.update(
+            WTF_CSRF_ENABLED=False,
+            DEBUG=True
+            )
+        return app
+
+    def setUp(self):
+        print("-----------")
+
+    def tearDown(self):
+        print("--------")
+
+class TestConvert(unittest.TestCase):
+
+    def test_values1(self):
+       self.assertEqual(birthDate(0), 'value entered is less than one month', msg='Equal')   
+
+    def test_values2(self):
+       self.assertEqual(birthDate(2022), '12', msg='Equal')
+
+    def test_values3(self):
+       self.assertEqual(birthDate("string"), 'ValueError: enter a number', msg='Equal')
+        
